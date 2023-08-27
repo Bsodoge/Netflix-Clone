159187f899bb972e1993d1149210f005462975d4
k74 <kashhh4u@gmail.com>
2023-08-27 14:18:05 +0100

    Add Link router over button

--- src/app/components/SignUp.tsx
+++ src/app/components/SignUp.tsx
@@ -1,9 +1,10 @@
 import styles from "./SignUp.module.css"
+import Link from "next/link"
 export default function SignUp(){
     return(
         <div className={styles.signup_container}>
           <p>Ready to watch? Press the sign up button to create an account</p>
-          <button>Sign up</button>
+          <Link href="/signin"><button>Sign up</button></Link>
         </div>
     )
 }
