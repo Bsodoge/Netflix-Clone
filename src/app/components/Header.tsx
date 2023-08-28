46d862749132632bb7a2d1c695dfe8287b22d562
k74 <kashhh4u@gmail.com>
2023-08-28 16:17:39 +0100

    Update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,27 @@
+'use client'
 import Betterflix from "../assets/Betterflix.svg"
 import Image from "next/image"
 import Link from "next/link"
 import styles from "./Header.module.css"
+import { useSession } from "next-auth/react"
+import { signOut } from "next-auth/react"
 
 export default function Header() {
+    const { data: session } = useSession();
+    const handleLogout = () => {
+        try {
+            signOut();
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
         <header className={styles.header}>
             <Link href={""}><Image src={Betterflix} alt="Netflix Logo" width={150} height={50}></Image></Link>
-            <Link href={""}><button className={styles.button}>Sign in</button></Link>
+            {
+                session ? <button className={styles.button} onClick={() => handleLogout()}>Log out</button> :
+                <Link href={"/signin"}><button className={styles.button}>Sign in</button></Link>
+            }
         </header>
     )
 }
