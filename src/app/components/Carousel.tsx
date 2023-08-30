d50e768c03a2abe5fd1383800ed9d7707131e3a7
k74 <kashhh4u@gmail.com>
2023-08-30 17:03:15 +0100

    Update Carousel

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -2,16 +2,39 @@
 
 import styles from "./Carousel.module.css"
 import { ReactElement, useEffect, useRef, useState } from "react"
+import CarouselMovie from "./CarouselMovie";
+import { IMovieDetails } from "../interfaces/IMovieDetails";
 
+interface props {
+    type: string,
+    setCurrentMovie: Function
+}
 
-export default function Carousel(){
+export default function Carousel({ type, setCurrentMovie }: props) {
     const [loading, setLoading] = useState<boolean>(true);
     const slider = useRef<HTMLDivElement>(null);
     const progressBar = useRef<HTMLDivElement>(null);
+    let [movies, setMovies] = useState<Array<IMovieDetails>>([]);
     useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
+            }
+        };
+
+        fetch(`https://api.themoviedb.org/3/movie/${type}?language=en-US&page=1`, options)
+            .then(response => response.json())
+            .then(response => {
+                const results = response.results.splice(0,10);
+                setMovies(results);
+                setCurrentMovie(results[0]);
+            })
+            .catch(err => console.error(err));
         setLoading(loading => !loading);
     }, [])
-    const generateProgressBars = () : ReactElement[] => {
+    const generateProgressBars = (): ReactElement[] => {
         const sliderIndex = Number(getComputedStyle(slider.current!).getPropertyValue('--slider-index'));
         const itemsPerScreen = Number(getComputedStyle(slider.current!).getPropertyValue('--items-per-screen'));
         const progressBarItemCount = Math.ceil(slider.current!.children.length / itemsPerScreen);
@@ -25,38 +48,37 @@ export default function Carousel(){
         }
         return progressBars;
     }
-    const handleMove = (type : string) : void => {
+    const handleMove = (type: string): void => {
         const itemsPerScreen = Number(getComputedStyle(slider.current!).getPropertyValue('--items-per-screen'));
         const progressBarItemCount = Math.ceil(slider.current!.children.length / itemsPerScreen);
         const sliderIndex = Number(getComputedStyle(slider.current!).getPropertyValue('--slider-index'));
         if (type === 'right') {
-            if(sliderIndex + 1 >= progressBarItemCount){
+            if (sliderIndex + 1 >= progressBarItemCount) {
                 slider.current!.style.setProperty("--slider-index", String(0));
                 progressBar.current!.children[sliderIndex].classList.remove(`${styles.active}`);
                 progressBar.current!.children[0].classList.add(`${styles.active}`);
             } else {
-                console.log(sliderIndex)
                 slider.current!.style.setProperty("--slider-index", String(sliderIndex + 1));
                 progressBar.current!.children[sliderIndex].classList.remove(`${styles.active}`);
-                progressBar.current!.children[sliderIndex +1].classList.add(`${styles.active}`);
+                progressBar.current!.children[sliderIndex + 1].classList.add(`${styles.active}`);
             }
         }
-        else if(type === 'left'){
-            if(sliderIndex - 1 < 0){
-                slider.current!.style.setProperty("--slider-index", String(progressBarItemCount -1));
+        else if (type === 'left') {
+            if (sliderIndex - 1 < 0) {
+                slider.current!.style.setProperty("--slider-index", String(progressBarItemCount - 1));
                 progressBar.current!.children[sliderIndex].classList.remove(`${styles.active}`);
                 progressBar.current!.children[progressBarItemCount - 1].classList.add(`${styles.active}`);
             } else {
                 slider.current!.style.setProperty("--slider-index", String(sliderIndex - 1));
                 progressBar.current!.children[sliderIndex].classList.remove(`${styles.active}`);
-                progressBar.current!.children[sliderIndex -1].classList.add(`${styles.active}`);
+                progressBar.current!.children[sliderIndex - 1].classList.add(`${styles.active}`);
             }
         }
     }
-    return(
+    return (
         <div className={styles.carousel_container}>
             <div className={styles.header}>
-                <h3 className={styles.title}>Popular</h3>
+                <h3 className={styles.title}>What's {type.replace('_', ' ')}</h3>
                 <div className={styles.progress_bar} ref={progressBar}>
                     {
                         loading ? <></> : generateProgressBars().map(bar => bar)
@@ -70,18 +92,9 @@ export default function Carousel(){
                     </div>
                 </button>
                 <div className={styles.slider} ref={slider}>
-                    <img src="https://via.placeholder.com/210/00FF00?text=1" />
-                    <img src="https://via.placeholder.com/220/00FF00?text=2" />
-                    <img src="https://via.placeholder.com/230/00FF00?text=3" />
-                    <img src="https://via.placeholder.com/240/00FF00?text=4" />
-                    <img src="https://via.placeholder.com/250/00FF00?text=5" />
-                    <img src="https://via.placeholder.com/260/00FF00?text=6" />
-                    <img src="https://via.placeholder.com/270/00FF00?text=7" />
-                    <img src="https://via.placeholder.com/280/00FF00?text=8" />
-                    <img src="https://via.placeholder.com/250/00FF00?text=9" />
-                    <img src="https://via.placeholder.com/260/00FF00?text=10" />
-                    <img src="https://via.placeholder.com/270/00FF00?text=11" />
-                    <img src="https://via.placeholder.com/280/00FF00?text=12" />
+                    {
+                        loading ? <></> : movies.map(movie => <CarouselMovie movieDetails={movie} setCurrentMovie={setCurrentMovie}/>)
+                    }
                 </div>
                 <button className={`${styles.handle} ${styles.right}`} onClick={() => handleMove('right')}>
                     <div className={styles.text}>
