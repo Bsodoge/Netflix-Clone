e61e751a3c2cf4b7e3cb39bc599325f25b149235
k74 <kashhh4u@gmail.com>
2023-08-31 22:13:10 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from './components/Footer'
 import SessionProvider from './SessionProvider'
+import Header from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <SessionProvider>   
+        <Header></Header>
           {children}
         </SessionProvider>
         <Footer></Footer>
