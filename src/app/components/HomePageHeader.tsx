8968648393093162f02f9c28e020d60d577ed012
k74 <kashhh4u@gmail.com>
2023-08-31 22:10:48 +0100

    Create HomePageHeader.tsx

---
+++ src/app/components/HomePageHeader.tsx
@@ -0,0 +1,29 @@
+'use client'
+import Betterflix from "../assets/Betterflix.svg"
+import Image from "next/image"
+import Link from "next/link"
+import styles from "./Header.module.css"
+import { useSession } from "next-auth/react"
+import { signOut } from "next-auth/react"
+import { usePathname } from 'next/navigation';
+
+export default function HomePageHeader() {
+    const { data: session } = useSession();
+    const pathName = usePathname();
+    const handleLogout = () => {
+        try {
+            signOut();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return(
+        <header className={styles.header}>
+            <Link href={""}><Image src={Betterflix} alt="Netflix Logo" width={150} height={50}></Image></Link>
+            {
+                session ? <button className={styles.button} onClick={() => handleLogout()}>Log out</button> :
+                <Link href={"/signin"}><button className={styles.button}>Sign in</button></Link>
+            }
+        </header>
+    )
+}
