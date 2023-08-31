18bc5f98bf0213f25779fb0bc939bede3c49e6d8
k74 <kashhh4u@gmail.com>
2023-08-31 22:11:22 +0100

    Update signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,14 +1,7 @@
-import Betterflix from "../assets/Betterflix.svg"
-import Link from "next/link"
-import Image from "next/image"
-import styles from "./page.module.css"
 import SigninForm from "./SigninForm"
 export default function Signin(){
     return(
         <main>
-            <header className={styles.header}>
-                <Link href={""}><Image src={Betterflix} alt="Netflix Logo" width={150} height={50}></Image></Link>
-            </header>
             <SigninForm />
         </main>
     )
