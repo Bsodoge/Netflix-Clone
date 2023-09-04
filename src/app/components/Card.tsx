a54ed0d5fdee6ee672ce7cdee1957f2d758edaac
k74 <kashhh4u@gmail.com>
2023-09-04 19:50:59 +0100

    Add card wrapper

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,11 +3,13 @@ import Link from "next/link"
 
 export default function Card() {
     return (
-        <div className={styles.card}>
-            <div className={styles.text_container}>
-                <h2>The Betterflix you love for just £4.20.</h2>
-                <p>Get the standard with advert plans.</p>
-                <Link href={""} className={styles.card_link}>Learn More</Link>
+        <div className={styles.card_container}>
+            <div className={styles.card}>
+                <div className={styles.text_container}>
+                    <h2>The Betterflix you love for just £4.20.</h2>
+                    <p>Get the standard with advert plans.</p>
+                    <Link href={""} className={styles.card_link}>Learn More</Link>
+                </div>
             </div>
         </div>
     )
